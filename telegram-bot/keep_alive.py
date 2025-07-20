e75c8cc26cd91005aef8983c9efdc227c51197e0
srikanth pata <154123278+srikanthsri1729@users.noreply.github.com>
2025-07-20 15:14:09 +0530

    Create keep_alive.py

---
+++ telegram-bot/keep_alive.py
@@ -0,0 +1,14 @@
+# keep_alive.py
+
+from flask import Flask
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+
+def keep_alive():
+    app.run(host='0.0.0.0', port=8080)
